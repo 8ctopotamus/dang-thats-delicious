2581e8092586056262ef56336b03504325ef5517
Josh Naylor <joshnaylor88@gmail.com>
2017-06-13 12:59:59 -0500

    can filter by tags

--- models/Store.js
+++ models/Store.js
@@ -46,13 +46,23 @@ storeSchema.pre('save', async function(next) {
 
   //find other stores that have slug of example, example-1, example-2
   const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i')
+
   const storesWithSlug = await this.constructor.find({ slug: slugRegEx })
+
   if (storesWithSlug.length){
-      this.slug = `${this.slug}-${storesWithSlug.length + 1}`
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`
   }
 
   next()
   //TODO make more resiliant so slugs are unique
 })
+
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } }},
+    { $sort: { count: -1 }}
+  ])
+}
 
 module.exports = mongoose.model('Store', storeSchema)
